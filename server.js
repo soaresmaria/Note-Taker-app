2695e687e7358f51407226dd124f84e5ecafc915
Mona Lisa <soaresmaria181@yahoo.com>
2021-09-05 03:31:42 -0400

    updated route req and post data

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const util = require('util');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 // parse incoming string or array data
@@ -13,6 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
+
+app.get("/notes", (req,res)=>{
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 app.get("/api/notes", (req,res)=>{
 
@@ -27,6 +32,7 @@ app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+// Function the save the note
 app.post("/api/notes", (req,res)=>{
     let newNote = req.body;
    
@@ -35,7 +41,21 @@ app.post("/api/notes", (req,res)=>{
         if(err) console.log(err);
         return Promise.resolve(JSON.parse(result));               
     })
-    
+    .then(data =>{
+     
+        newNote.id = getLastIndex(data) + 1;
+        
+        (data.length > 0)? data.push(newNote):data = [newNote];
+        return Promise.resolve(data);
+    })
+    .then(data =>{
+        // Function to write the new file
+        writeFileAsync("./db/db.json", JSON.stringify(data));
+        res.json(newNote);
+    })
+    .catch(err =>{
+        if(err) throw err;
+    });
 });
 
 
