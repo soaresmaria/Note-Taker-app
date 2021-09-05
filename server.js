cff7fb8596db48a70332d534e9cefad0d4712c71
Mona Lisa <soaresmaria181@yahoo.com>
2021-09-05 02:56:50 -0400

    defined a route for post requests and function added to parse incoming data

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const fs = require('fs');
+const util = require('util');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const readFileAsync = util.promisify(fs.readFile);
+
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 app.get("/api/notes", (req,res)=>{
@@ -11,6 +20,9 @@ app.get("/api/notes", (req,res)=>{
         return res.json(JSON.parse(result));       
     });     
 });
+
+app.post("/api/notes", (req,res) => {});
+
 
 
 app.listen(PORT, function(){
