e346598c8cf581c24c7dcc136a9238ffcd4a964a
Mona Lisa <soaresmaria181@yahoo.com>
2021-09-05 02:30:20 -0400

    added get request

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
+app.get("/api/notes", (req,res)=>{
+
+    readFileAsync("./db/db.json", "utf8")
+    .then((result, err)=>{
+        if(err) console.log(err);       
+        return res.json(JSON.parse(result));       
+    });     
+});
+
+
 app.listen(PORT, function(){
     console.log(`Listening on PORT ${PORT}`);
 });
